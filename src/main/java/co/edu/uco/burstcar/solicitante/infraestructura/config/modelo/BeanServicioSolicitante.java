d0e3af6acdb5ca63d559eb38bae80bf09faed4d8
Santiago CF <santicarfranco8@gmail.com>
2025-04-17 23:47:41 -0500

    Se realizan mejoras, nuevos endpoints necesarios y mejoras en la validaciones

--- src/main/java/co/edu/uco/burstcar/solicitante/infraestructura/config/modelo/BeanServicioSolicitante.java
+++ src/main/java/co/edu/uco/burstcar/solicitante/infraestructura/config/modelo/BeanServicioSolicitante.java
@@ -1,6 +1,7 @@
 package co.edu.uco.burstcar.solicitante.infraestructura.config.modelo;
 
 import co.edu.uco.burstcar.solicitante.dominio.puerto.RepositorioSolicitante;
+import co.edu.uco.burstcar.solicitante.dominio.servicio.solicitante.ServicioConsultarSolicitante;
 import co.edu.uco.burstcar.solicitante.dominio.servicio.solicitante.ServicioRegistrarSolicitante;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,5 +12,10 @@ public class BeanServicioSolicitante {
     @Bean
     public ServicioRegistrarSolicitante servicioRegistrarSolicitante(RepositorioSolicitante repositorioSolicitante){
         return new ServicioRegistrarSolicitante(repositorioSolicitante);
+    }
+
+    @Bean
+    public ServicioConsultarSolicitante servicioConsultarSolicitante(RepositorioSolicitante repositorioSolicitante){
+        return new ServicioConsultarSolicitante(repositorioSolicitante);
     }
 }
